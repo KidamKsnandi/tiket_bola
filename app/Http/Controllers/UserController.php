b0e992083d73e6d23229dae7e351597bbf0a02b4
KidamKsnandi <kidamkusnandi86@gmail.com>
2024-06-20 11:25:14 +0700

    add create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,30 @@ use App\Models\User;
 
 class UserController extends Controller
 {
+    public function create()
+    {
+        $role = Role::all();
+        return view('user.create', compact('role'));
+    }
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);  // Gunakan Hash::make untuk hashing password
+        $user->role_id = $request->role;
+        $user->save();
+
+        return redirect('/user')->with('success', 'User created successfully!');
+    }
+
     public function destroy($id)
     {
         $User = User::find($id);
