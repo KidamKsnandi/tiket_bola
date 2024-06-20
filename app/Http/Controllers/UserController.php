83e1fe184767e2a7968c180ff2edea7078549d8c
KidamKsnandi <kidamkusnandi86@gmail.com>
2024-06-20 11:06:15 +0700

    merge user/create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,4 +18,38 @@ class UserController extends Controller
 
         return view('users.index', $data);
     }
+
+    public function create()
+    {
+        $role = Role::all();
+        return view('user.create', compact('role'));
+    }
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);  // Gunakan Hash::make untuk hashing password
+        $user->role_id = $request->role;
+        $user->save();
+
+        return redirect('/user')->with('success', 'User created successfully!');
+    }
+
+    public function edit($id)
+    {
+    }
+    public function update($id, Request $request_)
+    {
+    }
+    public function destroy($id)
+    {
+    }
 }
