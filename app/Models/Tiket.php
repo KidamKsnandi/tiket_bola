a95d1bcf7813fe0addc46b8c3422bf685f506e66
KidamKsnandi <kidamkusnandi86@gmail.com>
2024-07-09 22:03:32 +0700

    setting tiket

--- app/Models/Tiket.php
+++ app/Models/Tiket.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tiket extends Model
 {
     use HasFactory;
+
+    public function jadwal_pertandingan()
+    {
+        return $this->belongsTo(JadwalPertandingan::class, 'id_jadwal_pertandingan');
+    }
 }
