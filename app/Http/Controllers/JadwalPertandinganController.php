9026152113a8f537a4984ff807239ea087d230f5
irwanadhiyanto <Irwan.adhiyanto@gmail.com>
2024-07-09 21:52:42 +0700

    edit jadwal pertandingan

--- app/Http/Controllers/JadwalPertandinganController.php
+++ app/Http/Controllers/JadwalPertandinganController.php
@@ -63,17 +63,36 @@ class JadwalPertandinganController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(JadwalPertandingan $jadwalPertandingan)
+    public function edit($id)
     {
-        //
+        $jadwalPertandingan = JadwalPertandingan::find($id);
+        $club = Club::all();
+        $stadion = Stadion::all();
+        return view("admin.jadwal_pertandingan.edit", compact("jadwalPertandingan", "club", "stadion"));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, JadwalPertandingan $jadwalPertandingan)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'club1' => 'required',
+            'club2' => 'required',
+            'keterangan' => 'required',
+            'jadwal_tanding' => 'required',
+            'stadion' => 'required',
+        ]);
+
+        $jadwalPertandingan = JadwalPertandingan::findOrFail($id);
+        $jadwalPertandingan->id_club_1 = $request->club1;
+        $jadwalPertandingan->id_club_2 = $request->club2;
+        $jadwalPertandingan->keterangan = $request->keterangan;
+        $jadwalPertandingan->tanggal_tanding = $request->jadwal_tanding;
+        $jadwalPertandingan->id_stadion = $request->stadion;
+        $jadwalPertandingan->save();
+
+        return redirect()->route('jadwal-pertandingan.index')->with('success', 'Jadwal Pertandingan updated successfully');
     }
 
     /**
--- resources/views/admin/jadwal_pertandingan/edit.blade.php
+++ resources/views/admin/jadwal_pertandingan/edit.blade.php
@@ -1,4 +1,57 @@
 @extends('layouts.admin.index')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <div class="card-header">Edit Jadwal Pertandingan</div>
+                    <div class="card-body">
+                        <form action="{{ route('jadwal-pertandingan.update', $jadwalPertandingan->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <label for="club1">Club 1:</label>
+                                <select name="club1" id="club1" class="form-control">
+                                    @foreach ($club as $c)
+                                        <option value="{{ $c->id }}"
+                                            @if ($c->id == $jadwalPertandingan->id_club_1) selected @endif>{{ $c->nama }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="club2">Club 2:</label>
+                                <select name="club2" id="club2" class="form-control">
+                                    @foreach ($club as $c)
+                                        <option value="{{ $c->id }}"
+                                            @if ($c->id == $jadwalPertandingan->id_club_2) selected @endif>{{ $c->nama }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="keterangan">Keterangan:</label>
+                                <input type="text" name="keterangan" id="keterangan" class="form-control"
+                                    value="{{ $jadwalPertandingan->keterangan }}">
+                            </div>
+                            <div class="form-group">
+                                <label for="jadwal_tanding">Jadwal Tanding:</label>
+                                <input type="datetime-local" name="jadwal_tanding" id="jadwal_tanding" class="form-control"
+                                    value="{{ date('Y-m-d\TH:i', strtotime($jadwalPertandingan->tanggal_tanding)) }}">
+                            </div>
+                            <div class="form-group">
+                                <label for="stadion">Stadion:</label>
+                                <select name="stadion" id="stadion" class="form-control">
+                                    @foreach ($stadion as $s)
+                                        <option value="{{ $s->id }}"
+                                            @if ($s->id == $jadwalPertandingan->id_stadion) selected @endif>{{ $s->nama }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
