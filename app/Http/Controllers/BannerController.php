bf826c533dbc4d78eb532ae86af7a6db1a09396e
irwanadhiyanto <Irwan.adhiyanto@gmail.com>
2024-07-16 23:15:39 +0700

    edit banner

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -44,7 +44,7 @@ class BannerController extends Controller
      */
     public function edit(Banner $banner)
     {
-        //
+        return view('admin.banners.edit', compact('banner'));
     }
 
     /**
@@ -52,7 +52,25 @@ class BannerController extends Controller
      */
     public function update(Request $request, Banner $banner)
     {
-        //
+        $validatedData = $request->validate([
+            'nama' => 'required|max:255',
+            'logo' => 'nullable|image|mimes:jpg,jpeg,png',
+            'deskripsi' => 'required',
+        ]);
+
+        $banner = Banner::find($banner->id);
+        $banner->nama = $validatedData['nama'];
+        if ($request->hasFile('gambar')) {
+            $banner->deleteGambar();
+            $image = $request->gambar;
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('images/banners/', $name);
+            $banner->gambar = $name;
+        }
+        $banner->deskripsi = $validatedData['deskripsi'];
+        $banner->save();
+
+        return redirect()->route('banner.index')->with('status', 'Banner updated successfully!');
     }
 
     /**
--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Banner extends Model
 {
     use HasFactory;
+
+    public function gambar()
+    {
+        if ($this->gambar && file_exists(public_path('images/banners/' . $this->gambar))) {
+            return asset('images/banners/' . $this->gambar);
+        } else {
+            return asset('images/no_image.png');
+        }
+    }
+
+    public function deletegambar()
+    {
+        if ($this->gambar && file_exists(public_path('images/banners/' . $this->gambar))) {
+            return unlink(public_path('images/banners/' . $this->gambar));
+        }
+    }
 }
